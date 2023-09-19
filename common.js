c50be7ccd94384a6916676460569dff92c950810
HikaruKubota <sakaku193@gmail.com>
2023-09-19 16:47:01 +0900

    init

---
+++ common.js
@@ -0,0 +1,130 @@
+const commonPublicFunctions = () => {
+  const publicFunctions = {
+    HOUR_MILLISECOND: 1000*60*60,
+    getCurrentDate: () => {
+      return new Date();
+    },
+    getNext20Date: () => {
+      const today = new Date();
+      if (today.getDate() > 20) {
+        today.setMonth(today.getMonth() + 1);
+      }
+      today.setDate(20);
+      return today;
+    },
+    getLastDate: (yyyy, mm) => {
+      const today = new Date();
+      if(yyyy) today.setFullYear(yyyy);
+      if(mm) today.setMonth(mm - 1);
+
+      const endOfTheMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+      return endOfTheMonth;
+    },
+    // 今週の日曜日
+    getFirstDayOfTheCurrentWeek: () => {
+      const today = new Date();
+      today.setDate(today.getDate() - today.getDay());
+      return today;
+    },
+    // 来週の日曜日
+    getFirstDayOfTheNextWeek: () => {
+      const today = new Date();
+      const result = new Date(today.getFullYear(), today.getMonth(), today.getDate() - today.getDay() + 7)
+      return result;
+    },
+    // 4週間後の火曜日
+    getTuesdayOfThe4WeeksLater: () => {
+      const today = new Date();
+      const result = new Date(today.getFullYear(), today.getMonth(), today.getDate() - today.getDay() + 2 + (7 * 4))
+      return result;
+    },
+    // 来月の第3週目の水曜日
+    getThirdWednesdayOfTheNextMonth: () => {
+      const today = new Date();
+      // 来月月初
+      const b = new Date(today.getFullYear(), today.getMonth() + 1, 1)
+      // 来月月初の週の日曜日
+      const e = new Date(b.getFullYear(), b.getMonth(), b.getDate() - b.getDay())
+      // 来月の第三水曜日
+      const r = new Date(e.getFullYear(), e.getMonth(), e.getDate() + 3 + (7 * 2))
+
+      return r
+    },
+    // 最終週の水曜日を返す※当月ではなく、該当の日付から見た最終週の水曜日
+    // セルに記載されてる日付から過ぎた場合に起動するので、当月の最終週の水曜日を返す方針にする。
+
+
+    getMondayOfTheCurrentWeek: () => {
+      const today = new Date();
+      today.setDate(today.getDate() - today.getDay() + 1);
+      return today;
+    },
+    getEndDayOfTheCurrentWeek: () => {
+      const today = new Date();
+      today.setDate(today.getDate() - today.getDay() + 6);
+      return today;
+    },
+    sendMessageToDiscord: (message) => {
+      const WEBHOOK_URL = "https://discord.com/api/webhooks/1139086345801834536/wdu_FCzPX0uQrUKqvgUr2fTVCmUIrNQ_KOreCFxUQmDZ3cWOWnWzQFXQora4zKecAgql";
+
+      const payload = {
+        username: 'ログレススケジュール',
+        content: message,
+      };
+
+      UrlFetchApp.fetch(WEBHOOK_URL, {
+        method: "post",
+        contentType: "application/json",
+        payload: JSON.stringify(payload),
+      });
+    },
+
+    findColumnByHeader: (sheet, val) => {
+      const last_col = sheet.getLastColumn();
+      const range = sheet.getRange(1, 1, 1, last_col);
+      const headers = range.getValues().flat();
+
+      for(let i=0;i<headers.length;i++){
+        if(headers[i] === val){
+          return i+1;
+        }
+      }
+      return 0;
+    },
+  }
+  return publicFunctions;
+}
+
+
+// 存在する日付かをチェックする
+function ckDate(strDate) {
+  var y = strDate.split("/")[0];
+  var m = strDate.split("/")[1] - 2;
+  var d = strDate.split("/")[2];
+  var date = new Date(y,m,d);
+  if(date.getFullYear() != y || date.getMonth() != m || date.getDate() != d){
+    return false;
+  }
+  return true;
+}
+
+// 指定したシートを指定したbookにコピーして一番左へ移動
+// ついでにシート１も削除
+function copySheetToSpreadSheets(sheet, spreadSheetsId){
+  let destSpreadsheet = SpreadsheetApp.openById(spreadSheetsId);
+  let newCopySheet = sheet.copyTo(destSpreadsheet);
+  newCopySheet.setName(sheet.getSheetName());
+  if(destSpreadsheet.getSheetByName("シート1")){
+    destSpreadsheet.deleteSheet(destSpreadsheet.getSheetByName("シート1"));
+  }
+}
+
+// 指定したフォルダーに名前を指定して新しいbookを作成
+function createSpreadsheetInfolder(folderID, fileName) {
+  var folder = DriveApp.getFolderById(folderID);
+  var newSS=SpreadsheetApp.create(fileName);
+  var originalFile=DriveApp.getFileById(newSS.getId());
+  var copiedFile = originalFile.makeCopy(fileName, folder);
+  originalFile.setTrashed(true);
+  return copiedFile;
+}
---
+++ cron.js
@@ -0,0 +1,396 @@
+/**
+ * 毎日8~22時に起動するTrigger
+ * 開催されている時限イベントをチェックし、通知する。
+ */
+const noticeTimedEventsMain = () => {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('石回収管理表');
+  const common = commonPublicFunctions();
+
+  /**
+   * 現在開催中の行を取得
+   */
+  const timedEventColumn = common.findColumnByHeader(sheet, '開催時間')
+  const lastRow = sheet.getMaxRows();
+  const timedEvents = sheet.getRange(1, timedEventColumn, lastRow, 1).getValues();
+
+  const today = new Date()
+  const nowHours = today.getHours()
+
+  const nowTimedEventsRows = timedEvents.map((e, i) => {
+    if(e == '' || e == '開催時間' || e == '常時') return undefined
+    // sample: e = [8:00～8:59 / 12:00～12:59 / 20:00～20:59 / 24:00～24:59]
+    const targetHours = e[0].split('/').map(ee => ee.split(':')[0])
+    console.log(targetHours)
+    const result = targetHours.find(ee => ee == nowHours)
+    return result ? i + 1 : undefined
+  }).filter(e => {
+    return e
+  })
+
+  /**
+   * Rowsから何が今日が期限かを取得する。
+   */
+  const detailColumn = common.findColumnByHeader(sheet, '詳細')
+  const itemColumn = common.findColumnByHeader(sheet, '中項目')
+  const nowTimedEventsDetails = nowTimedEventsRows.map((row) => {
+    return {
+      row: row,
+      item: sheet.getRange(row, itemColumn).getValue(),
+      detail: sheet.getRange(row, detailColumn).getValue()
+    }
+  })
+
+  /**
+   * メッセージ送信部分
+   */
+  const message = 
+`現在開催中のイベントです！
+
+${
+  nowTimedEventsDetails.length != 0 ? nowTimedEventsDetails.map(e => e.item + ' - ' + e.detail).reduce((a, c) => {
+  return a + '・' + c + '\n'
+  }, '') : ''
+}
+`
+  if(nowTimedEventsDetails.length != 0) common.sendMessageToDiscord(message)
+}
+
+
+/**
+ * 毎日5時に起動するTrigger
+ * 1.期限が今日の物をDiscordに通知
+ * todo: ほぼコピペで作ったため、かなりコードが被っている。
+ * refactoringする。
+ */
+const noticeDeadlinesMain = () => {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('石回収管理表');
+  const common = commonPublicFunctions();
+
+  /**
+   * 期限が今日のRowを取得
+   */
+  const deadlineColumn = common.findColumnByHeader(sheet, '期限')
+  const lastRow = sheet.getMaxRows();
+  const deadlines = sheet.getRange(1,deadlineColumn,lastRow,1).getValues();
+
+  const today = new Date()
+  const ck = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+  const deadRows = deadlines.map((e, i) => {
+    if(e == '' || e == '期限') return undefined
+    const targetDate = new Date(e)
+    const t_yyyy = targetDate.getFullYear();
+    const t_mm = ('00' + targetDate.getMonth()+1).slice(-2);
+    const t_dd = ('00' + targetDate.getDate()).slice(-2);
+    const c_yyyy = ck.getFullYear();
+    const c_mm = ('00' + ck.getMonth()+1).slice(-2);
+    const c_dd = ('00' + ck.getDate()).slice(-2);
+    const t = "" + t_yyyy + t_mm + t_dd
+    const c = "" + c_yyyy + c_mm + c_dd
+    console.log({t})
+    console.log({c})
+    return +t == +c ? i + 1 : undefined
+  }).filter(e => {
+    return e
+  })
+
+  /**
+   * Rowsから何が今日が期限かを取得する。
+   */
+  const detailColumn = common.findColumnByHeader(sheet, '詳細')
+  const itemColumn = common.findColumnByHeader(sheet, '中項目')
+  const deadDetails = deadRows.map((deadRow) => {
+    return {
+      row: deadRow,
+      item: sheet.getRange(deadRow, itemColumn).getValue(),
+      detail: sheet.getRange(deadRow, detailColumn).getValue()
+    }
+  })
+
+  /**
+   * メッセージ送信部分
+   */
+  const message = 
+`本日、期限の物は下記となります。
+
+${
+  deadDetails.length != 0 ? deadDetails.map(e => e.item + ' - ' + e.detail).reduce((a, c) => {
+  return a + '・' + c + '\n'
+  }, '') : '本日、期限の物はありません。\n'
+}
+https://docs.google.com/spreadsheets/d/1DHWZMRe7utMagIqEP7YJr3Yqe7PrFGpsA44PXprL6ik/edit#gid=0&fvid=671968663
+`
+  common.sendMessageToDiscord(message)
+}
+
+/**
+ * 毎日3時に起動するTrigger
+ * 1.期限が切れている物の期限を更新
+ *  ・神獣：期限＋３日
+ *  ・王国勲章：次の週末、月末
+ *  ・属性試練,クロノスの試練：来月の第三水曜日
+ *  ・宝石の神獣：XXXXちょっとめんどくさいので考える。次の最終週の水曜日
+ *  ・六花の試練：４週間後の水曜日
+ *  ・期限を更新した物は全て未完了にする。
+ */
+const updateDeadlinesMain = () => {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('石回収管理表');
+  const common = commonPublicFunctions();
+
+  /**
+   * 期限の切れているRowを取得
+   */
+  const deadlineColumn = common.findColumnByHeader(sheet, '期限')
+  const lastRow = sheet.getMaxRows();
+  const deadlines = sheet.getRange(1,deadlineColumn,lastRow,1).getValues();
+
+  const today = new Date()
+  const ck = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1)
+  const deadRows = deadlines.map((e, i) => {
+    if(e == '' || e == '期限') return undefined
+    const targetDate = new Date(e)
+    const t_yyyy = targetDate.getFullYear();
+    const t_mm = ('00' + targetDate.getMonth()+1).slice(-2);
+    const t_dd = ('00' + targetDate.getDate()).slice(-2);
+    const c_yyyy = ck.getFullYear();
+    const c_mm = ('00' + ck.getMonth()+1).slice(-2);
+    const c_dd = ('00' + ck.getDate()).slice(-2);
+    const t = "" + t_yyyy + t_mm + t_dd
+    const c = "" + c_yyyy + c_mm + c_dd
+    console.log({t})
+    console.log({c})
+    return +t <= +c ? i + 1 : undefined
+  }).filter(e => {
+    return e
+  })
+
+  /**
+   * 期限切れのRowから何が期限切れかを取得する。
+   * 9月3日
+   * 詳細に被りがないため、詳細の文言で判定する。
+   */
+  const classificationColumn = common.findColumnByHeader(sheet, '分類')
+  const itemColumn = common.findColumnByHeader(sheet, '中項目')
+  const detailColumn = common.findColumnByHeader(sheet, '詳細')  
+
+  const deadDetails = deadRows.map((deadRow) => {
+    return {
+      row: deadRow,
+      classification: sheet.getRange(deadRow, classificationColumn).getValue(),
+      item: sheet.getRange(deadRow, itemColumn).getValue(),
+      detail: sheet.getRange(deadRow, detailColumn).getValue()
+    }
+  })
+
+  /**
+   * 更新処理呼び出し
+   */
+  updateDeadlines(sheet, deadDetails, deadlineColumn)
+
+  /**
+   * メッセージ送信部分
+   */
+  const message = 
+`スケジュールを更新します。
+下記が更新対象です。
+
+${
+  deadDetails.length != 0 ? deadDetails.map(e => e.item + ' - ' + e.detail).reduce((a, c) => {
+  return a + '・' + c + '\n'
+  }, '') : '更新対象はありません。\n'
+}
+https://docs.google.com/spreadsheets/d/1DHWZMRe7utMagIqEP7YJr3Yqe7PrFGpsA44PXprL6ik/edit#gid=0&fvid=671968663
+`
+  common.sendMessageToDiscord(message)
+}
+
+/**
+ * 更新処理呼び出し部分
+ */
+const updateDeadlines = (sheet, deadDetails, deadlineColumn) => {
+  deadDetails.forEach(e => {
+    if(e.item === '神獣討伐令' && e.detail === '神獣') updateDivineBastsDeadline(sheet, e.row, deadlineColumn)
+    if(e.item === '王国勲章' && e.detail === 'ウィークリー') updateRoyalOrderWeeklyDeadline(sheet, e.row, deadlineColumn)
+    if(e.item === '六花の試練 part1' || e.item === '六花の試練 part2') {
+      updateTheSixFlowerTrialsDeadline(sheet, e.row, deadlineColumn)
+      updateTheSixFlowerTrialsStatus(sheet, e.item)
+    }
+    if(e.item === '属性の試練' || e.item === 'クロノスの試練') updateAttributesDeadline(sheet, e.row, deadlineColumn)
+    if(e.classification === 'イベント') updateEventDeadline(sheet, e.row, deadlineColumn)
+  })
+}
+
+/**
+ * 神獣締め切り更新
+ */
+const updateDivineBastsDeadline = (sheet, row, column) => {
+  const deadline = new Date(sheet.getRange(row, column).getValue())
+  const updateDeadline = new Date(deadline.getFullYear(), deadline.getMonth(), deadline.getDate() + 3)
+  sheet.getRange(row, column).setValue(updateDeadline)
+
+  updateState(sheet, row)
+}
+
+/**
+ * 王国勲章ウィークリー締め切り更新
+ * 月曜日に更新なので締め切りは日曜日
+ */
+const updateRoyalOrderWeeklyDeadline = (sheet, row, column) => {
+  const common = commonPublicFunctions();
+
+  const updateDeadline = common.getFirstDayOfTheNextWeek()
+  sheet.getRange(row, column).setValue(updateDeadline)
+
+  updateState(sheet, row)
+}
+
+/**
+ * 属性の試練、クロノスの試練更新
+ */
+const updateAttributesDeadline = (sheet, row, column) => {
+  const common = commonPublicFunctions();
+
+  const updateDeadline = common.getThirdWednesdayOfTheNextMonth()
+  sheet.getRange(row, column).setValue(updateDeadline)
+
+  updateState(sheet, row)
+}
+
+/**
+ * 宝石の守護神更新
+ */
+
+
+/**
+ * 六花の試練更新
+ * 期限の更新部分
+ * 2週間ごとに切り替わる。
+ * 期限が切れた行が次期限切れになるのは4週間後
+ */
+const updateTheSixFlowerTrialsDeadline = (sheet, row, column) => {
+  const common = commonPublicFunctions();
+
+  const updateDeadline = common.getTuesdayOfThe4WeeksLater()
+  sheet.getRange(row, column).setValue(updateDeadline)
+}
+
+/**
+ * 六花の試練更新
+ * ステータスの更新部分
+ * Part1が期限切れならPart2
+ * Part2が期限切れならPart1を未完了とする。
+ */
+const updateTheSixFlowerTrialsStatus = (sheet, item) => {
+  const common = commonPublicFunctions();
+  const targetItemLabel = item === '六花の試練 part1' ?
+    '六花の試練 part2' :
+    item === '六花の試練 part2' ? 
+      '六花の試練 part1' : undefined
+  if(!targetItemLabel) return
+
+  // 未完了にするカラムのrowを取得する。
+  const itemColumn = common.findColumnByHeader(sheet, '中項目')
+  const lastRow = sheet.getMaxRows();
+  const targetRows = sheet.getRange(1,itemColumn,lastRow,1).getValues().map((e, i) => {
+    return e === targetItemLabel ? i + 1 : undefined
+  }).filter(e => {
+    return e
+  })
+
+  targetRows.forEach(row => {
+    updateState(sheet, row)
+  })
+}
+
+/**
+ * 追憶のデイリー試練更新
+ */
+
+/**
+ * Event更新
+ * 期限の欄を空欄にするだけ
+ */
+const updateEventDeadline = (sheet, row, column) => {
+  sheet.getRange(row, column).setValue('')
+}
+
+// 未完了に変更
+const updateState = (sheet, row) => {
+  const common = commonPublicFunctions();
+
+  // 更新する対象を定義
+  const targets = ['おちゃ', 'おちゃ2nd']
+  targets.forEach((e) => {
+    const targetColumn = common.findColumnByHeader(sheet, e)
+    sheet.getRange(row, targetColumn).setValue('未完了')
+  })
+}
+
+
+/**
+ * cron系
+ */
+function getTriggers(){
+  const common = commonPublicFunctions();
+
+  // 8~23
+  const times = Array.from({ length: 16}, (e, index) => index + 8);
+  const noticeTimedEventsTriggers = times.map(time => {
+    return {
+      taskName: 'noticeTimedEventsMain',
+      schedule: {
+        date: common.getCurrentDate(),
+        hours: time,
+        minutes: 00,
+      }
+    }
+  })
+
+  const triggers = [
+    {
+      taskName: 'updateDeadlinesMain',
+      schedule: {
+        date: common.getCurrentDate(),
+        hours: 3,
+        minutes: 00,
+      }
+    },
+    {
+      taskName: 'noticeDeadlinesMain',
+      schedule: {
+        date: common.getCurrentDate(),
+        hours: 5,
+        minutes: 00,
+      }
+    },
+  ].concat(noticeTimedEventsTriggers);
+
+  console.log(triggers)
+
+  return triggers;
+}
+
+function setTrigger(){
+  const triggers = getTriggers();
+
+  deleteTriggers(triggers);
+  const time = new Date();
+  triggers.forEach((trigger) => {
+    time.setFullYear(trigger.schedule.date.getFullYear());
+    time.setMonth(trigger.schedule.date.getMonth());
+    time.setDate(trigger.schedule.date.getDate());
+    time.setHours(trigger.schedule.hours);
+    time.setMinutes(trigger.schedule.minutes);
+    ScriptApp.newTrigger(trigger.taskName).timeBased().at(time).create();
+  });
+}
+
+function deleteTriggers(crons) {
+  const triggers = ScriptApp.getProjectTriggers();
+  triggers.forEach(trigger => {
+    crons.forEach((cron) => {
+      if(trigger.getHandlerFunction() == cron.taskName){
+        ScriptApp.deleteTrigger(trigger);
+      }
+    })
+  })
+}
